56a4744264e3c9e33d8aa254b47cb7dc2df3fb30
Pallab145 <156820475+Pallab145@users.noreply.github.com>
2024-12-17 15:16:13 +0530

    Update socket.ts

--- apps/frontend/components/videocall/socket.ts
+++ apps/frontend/components/videocall/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:5000"; 
+const URL = "https://p2-p-meet-backend.vercel.app/"; 
 const socket = io(URL);
 
 socket.on('connection-success',({ socketId }) => {
